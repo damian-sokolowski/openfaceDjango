10529e95fca23c8f7fc3b719663b1ae1e19eb82b
Damian Sokolowski <damian.sokolowski@doradigital.pl>
2016-06-15 16:37:49 +0200

    Created model for app webopenface

--- webopenface/models.py
+++ webopenface/models.py
@@ -2,4 +2,47 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    uid = models.CharField(max_length=50, )
+    name = models.CharField(max_length=50, )
+    add_date = models.DateTimeField(auto_now_add=False, )
+    mod_date = models.DateTimeField(auto_now=False, )
+
+    class Meta:
+        verbose_name_plural = "people"
+
+    def __unicode__(self):
+        return self.name
+
+
+class PeopleFace(models.Model):
+    person = models.ForeignKey(Person, )
+    face = models.TextField()
+
+    def __unicode__(self):
+        return self.person.name
+
+
+class Frame(models.Model):
+    frame = models.TextField()
+    add_date = models.DateTimeField(auto_now_add=False, )
+
+    def __unicode__(self):
+        return self.frame
+
+
+class DetectedFace(models.Model):
+    frame = models.ForeignKey(Frame)
+    face = models.TextField()
+
+    def __unicode__(self):
+        return self.face
+
+
+class DetectedPeople(models.Model):
+    face = models.ForeignKey(DetectedFace, )
+    person = models.ForeignKey(Person, )
+
+    def __unicode__(self):
+        return self.person.name
