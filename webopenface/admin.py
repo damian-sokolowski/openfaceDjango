d2ded19249940f7bd689f45a7a7fd57894682abe
Damian Sokolowski <damian.sokolowski@doradigital.pl>
2016-06-16 09:43:18 +0200

    Changes in the admin panel

--- webopenface/admin.py
+++ webopenface/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from webopenface.models import Person, PeopleFace, DetectedPeople, DetectedFace, Frame
 
-# Register your models here.
+
+class PersonAdmin(admin.ModelAdmin):
+    date_hierarchy = 'add_date'
+    list_filter = ['add_date']
+    list_display = ('name', 'uid', 'mod_date')
+    fieldsets = [
+        (None,               {'fields': ['uid', 'name']}),
+        ('Date information', {'fields': ['add_date', 'mod_date']}),
+    ]
+
+
+class PeopleFaceAdmin(admin.ModelAdmin):
+    list_display = ('person', 'face')
+
+
+class DetectedPeopleAdmin(admin.ModelAdmin):
+    list_display = ('person', 'face')
+
+
+class FrameAdmin(admin.ModelAdmin):
+    date_hierarchy = 'add_date'
+    list_filter = ['add_date']
+    list_display = ('frame', 'add_date')
+
+admin.site.register(Person, PersonAdmin)
+admin.site.register(PeopleFace, PeopleFaceAdmin)
+admin.site.register(DetectedPeople, DetectedPeopleAdmin)
+admin.site.register(DetectedFace)
+admin.site.register(Frame, FrameAdmin)
