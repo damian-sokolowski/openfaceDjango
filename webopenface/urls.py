a45a34e959f349c6d2ef642d74d72a9015868646
Damian Sokolowski <damian.sokolowski@doradigital.pl>
2016-07-06 20:10:22 +0200

    Changed configuration URL

--- webopenface/urls.py
+++ webopenface/urls.py
@@ -1,9 +1,11 @@
-from django.conf.urls import url
-
+from django.conf.urls import include, url
 from webopenface import views
 
 urlpatterns = [
+    url(r'^api/', include('webopenface.api_urls')),
     url(r'^$', views.index, name='index'),
     url(r'^preview/$', views.preview, name='preview'),
     url(r'^person/$', views.person, name='person'),
+    url(r'^lastframes/$', views.last_frames, name='lastFrames'),
+    url(r'^detectedpeople/(?P<pk>[0-9])$', views.detected_people, name='detectedPeople'),
 ]
