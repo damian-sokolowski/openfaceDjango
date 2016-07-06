3634973887a71c7c01d1cf41f7ca2c6d17372a69
Damian Sokolowski <damian.sokolowski@doradigital.pl>
2016-07-06 20:19:37 +0200

    Added views for applications webopenface

--- webopenface/views.py
+++ webopenface/views.py
@@ -1,17 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from webopenface.models import DetectedPeople, DetectedFace
+from webopenface.models import Person, DetectedPeople, DetectedFace, Frame
 
 
 def index(request):
-    preview_list = DetectedPeople.objects.order_by('face__frame__add_date')
-    output = ', '.join(i.person.name for i in preview_list)
-    return HttpResponse(output)
+    added_people = Person.objects.order_by('name')
+    context = {'added_people': added_people}
+    return render(request, 'webopenface/index.html', context)
 
 
 def preview(request):
-    return HttpResponse("Hello, world. preview")
+    detected_people = []
+    latest_frame = Frame.objects.latest('add_date')
+    context = {
+        'latest_frame': latest_frame
+    }
+    return render(request, 'webopenface/preview.html', context)
 
 
 def person(request):
-    return HttpResponse("Hello, world. person")
+    return render(request, 'webopenface/person.html')
+
+
+def last_frames(request):
+    frames = Frame.objects.order_by('-add_date')[:10]
+    return render(request, 'webopenface/lastframes.html', {'frames': frames})
+
+
+def detected_people(request, pk):
+    detected = Frame.objects.get(pk=pk).detectedface_set.all()
+    context = {
+        'detected': detected
+    }
+    return render(request, 'webopenface/detected_people.html', context)
