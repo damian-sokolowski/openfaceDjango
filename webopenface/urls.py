82c938fb920a36fe1ff06be97b3018a7c7dfd1f5
Damian Sokolowski <dammansokol@gmail.com>
2016-07-26 14:46:59 +0200

    Added some views for debugging

--- webopenface/urls.py
+++ webopenface/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^preview/$', views.preview, name='preview'),
     url(r'^person/$', views.person, name='person'),
+    url(r'^lastframes/$', views.last_frames, name='lastFrames'),
+    url(r'^detectedpeople/(?P<pk>[0-9]*)$', views.detected_people, name='detectedPeople'),
+    url(r'^addedpeople/$', views.added_people, name='addedPeople'),
+    url(r'^addedpeople/(?P<pk>[0-9]*)$', views.person_details, name='personDetails'),
 ]
--- webopenface/views.py
+++ webopenface/views.py
@@ -24,3 +24,43 @@ def preview(request):
 
 def person(request):
     return render(request, 'webopenface/person.html')
+
+
+def last_frames(request):
+    return render(
+        request,
+        'webopenface/last_frames.html',
+        {
+            'frames': Frame.objects.order_by('-add_date').all()
+        }
+    )
+
+
+def detected_people(request, pk):
+    return render(
+        request,
+        'webopenface/detected_people.html',
+        {
+            'detected': Frame.objects.get(pk=pk).detectedface_set[:1000]
+        }
+    )
+
+
+def added_people(request):
+    return render(
+        request,
+        'webopenface/added_people.html',
+        {
+            'people': Person.objects.order_by('-add_date').all()
+        }
+    )
+
+
+def person_details(request, pk):
+    return render(
+        request,
+        'webopenface/person_details.html',
+        {
+            'person': Person.objects.get(pk=pk)
+        }
+    )
